470751930230ba8dd36810fd29a2c1148e552583
Wang Junbo <cr9294wjb@163.com>
2023-12-21 10:41:20 +0800

    提交

---
+++ 规划问题/16-21年的规划问题.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import csv
+import numpy as np
+
+
+def process_data(file_path):
+    df = pd.read_csv(file_path)
+    height, _ = df.shape
+    m = df.values
+    huangjin = [m[i][3] for i in range(height) if m[i][2] == 0]
+    return df, height, m, huangjin
+
+
+def calculate_asset_value_and_operations(df, height, m, huangjin):
+    # Initialization
+    k = 1  # k represents holding gold (2 for gold, 1 for bitcoin, 0 for cash)
+    n = 0  # Gold statistics
+    rateG = 0.01  # Gold transaction fee rate
+    rateB = 0.02  # Bitcoin transaction fee rate
+    oldprice_G = 0
+    oldprice_B = 0
+    newprice_G = 0
+    newprice_B = 0
+    target = 2000  # Initial assets
+    res = []  # Store daily operations
+
+    # Main Loop
+    for i in range(1, height - 1):
+        res1 = []
+
+        # Print current day's assets
+        print("Day %d's assets: %f" % (i, target))
+        res1.append(target)
+
+        # If gold cannot be traded
+        if m[i][2] == 1:
+            # Holding cash
+            if k == 0:
+                oldprice_B = m[i][1]
+                newprice_B = m[i + 1][1]
+                tmp = target / (1 + rateB) * newprice_B / oldprice_B
+                tmp = round(tmp, 2)
+                target1 = max(target, tmp)
+                k1 = 0 if target1 == target else 1
+
+            # Holding bitcoin
+            elif k == 1:
+                oldprice_B = m[i][1]
+                newprice_B = m[i + 1][1]
+                tmp1 = target * (newprice_B / oldprice_B)
+                tmp1 = round(tmp1, 2)
+                tmp2 = target / (1 + rateB)
+                tmp2 = round(tmp2, 2)
+                target1 = max(tmp1, tmp2)
+                k1 = 0 if target1 == tmp2 else 1
+
+            # Holding gold
+            elif k == 2:
+                target1 = target
+                k1 = 2
+
+        # If gold can be traded
+        elif m[i][2] == 0:
+            oldprice_B = m[i][1]
+            newprice_B = m[i + 1][1]
+            oldprice_G = huangjin[n]
+            newprice_G = huangjin[n + 1]
+
+            # Holding cash
+            if k == 0:
+                tmp1 = target / (1 + rateG) * newprice_G / oldprice_G
+                tmp1 = round(tmp1, 2)
+                tmp2 = target / (1 + rateB) * newprice_B / oldprice_B
+                tmp2 = round(tmp2, 2)
+                target1 = max(tmp1, tmp2, target)
+                k1 = 2 if target1 == tmp1 else 1 if target1 == tmp2 else 0
+
+            # Holding bitcoin
+            elif k == 1:
+                tmp1 = target * newprice_B / oldprice_B
+                tmp1 = round(tmp1, 2)
+                tmp2 = target / (1 + rateB)
+                tmp2 = round(tmp2, 2)
+                tmp3 = target / (1 + rateB + rateG) * newprice_G / oldprice_G
+                tmp3 = round(tmp3, 3)
+                target1 = max(tmp1, tmp2, tmp3)
+                k1 = 1 if target1 == tmp1 else 0 if target1 == tmp2 else 2
+
+            # Holding gold
+            elif k == 2:
+                tmp1 = target * newprice_G / oldprice_G
+                tmp1 = round(tmp1, 2)
+                tmp2 = target / (1 + rateG)
+                tmp2 = round(tmp2, 2)
+                tmp3 = target / (1 + rateB + rateG) * newprice_B / oldprice_B
+                tmp3 = round(tmp3, 3)
+                target1 = max(tmp1, tmp2, tmp3)
+                k1 = 2 if target1 == tmp1 else 0 if target1 == tmp2 else 1
+
+            n += 1
+
+        res1.extend([m[i][0], k1])
+        res.append(res1)
+        k = k1
+        target = target1
+
+    return np.array(res)
+
+
+def write_to_csv(results, output_file):
+    with open(output_file, 'w', encoding='utf-8', newline="") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(["资金", "日期(月/日/年)", "操作"])
+        csv_writer.writerows(results)
+
+
+if __name__ == "__main__":
+    file_path = "../C题处理后的中间文件2.csv"
+    df, height, m, huangjin = process_data(file_path)
+    results = calculate_asset_value_and_operations(df, height, m, huangjin)
+
+    # Output
+    print("Number of rows in the resulting array:", results.shape[0])
+    print(results)
+
+    output_file = '资产+每天的实际操作-2000-new.csv'
+    write_to_csv(results, output_file)
---
+++ 预测/ar.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+from statsmodels.tsa.arima.model import ARIMA
+import xlwt
+
+# Load your data
+Data = pd.read_csv("../C题处理后的中间文件2.csv")
+
+# Convert the date to a timestamp
+def to_timestamp(date):
+    return int(time.mktime(time.strptime(date, "%m/%d/%y")))
+
+start_timestamp = to_timestamp(Data.iloc[0, 0])
+for i in range(Data.shape[0]):
+    Data.iloc[i, 0] = (to_timestamp(Data.iloc[i, 0]) - start_timestamp) / 86400
+
+# Set the number of days for training
+days_fit = Data.shape[0]  # Minimum value is 2
+
+# Initialize ARIMA model for Bitcoin
+bitcoin_model = ARIMA(Data.iloc[0:days_fit, 1], order=(5, 1, 0))  # Replace p, d, q with appropriate values
+
+# Fit the ARIMA model
+bitcoin_fit = bitcoin_model.fit()
+
+# Forecasting
+bitcoin_forecast = bitcoin_fit.forecast(steps=days_fit)  # Adjust steps as needed
+print(bitcoin_forecast)
+
+# Prepare the results
+ji3 = np.round(bitcoin_forecast, 2)
+ji2 = Data.iloc[2:days_fit + 1, 1].values
+
+# Save the results to Excel
+book = xlwt.Workbook(encoding="utf-8", style_compression=0)
+sheet = book.add_sheet("ARIMA预测比特币", cell_overwrite_ok=True)
+col = ("日期", "预测值", "真实值", "误差")
+for i in range(0, 4):
+    sheet.write(0, i, col[i])
+
+print(ji3)
+print(ji2)
+print(Data.iloc[2:days_fit + 1, 0].values)
+# Fill in the sheet
+for i in range(0, days_fit - 1):
+    sheet.write(i + 1, 0, Data.iloc[i + 2, 0])
+    sheet.write(i + 1, 1, ji3[i][0])
+    sheet.write(i + 1, 2, ji2[i])
+    sheet.write(i + 1, 3, abs(ji3[i][0] - ji2[i]))
+
+
+# Save the workbook
+book.save("ARIMA预测比特币.xls")
